6f1ba752c2591924ba6a9cc8fb677ebaf56eed17
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-07-23 21:03:25 +0530

    string 1

---
+++ stringk.java
@@ -0,0 +1,16 @@
+public class stringk
+{
+  public static void main(String[] args)
+  {
+  
+
+  char arr[]={'a','b','c','d','e'};
+  String str="raghu";
+  String str1=new String("raghu");
+  System.out.println(arr);
+  System.out.println(str);
+  System.out.println(str1);
+  
+  
+    }
+}
